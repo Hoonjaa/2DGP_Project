b856d9c077b70075bd90511b6ce21c0eefe1e4c2
Hoonjaa <gnswp8978@gmail.com>
2025-10-18 20:18:28 +0900

    [Update / 25.10.18] main.py // 월드 리스트에 플레이어 추가

--- Project/main.py
+++ Project/main.py
@@ -1,15 +1,22 @@
 from pico2d import *
+from player import Player
 
 def reset_world():
     global world
 
     world = []
 
+    player = Player()
+    world.append(player)
+
 def update_world():
-    pass
+    for obj in world:
+        obj.update()
 
 def render_world():
     clear_canvas()
+    for obj in world:
+        obj.draw()
     update_canvas()
 
 
@@ -22,6 +29,6 @@ reset_world()
 while running:
     update_world()
     render_world()
-    delay(0.01)
+    delay(0.1)
 
 close_canvas()
