f29614608a5364cc922be7bfd700abf48430ec16
Hoonjaa <gnswp8978@gmail.com>
2025-10-18 21:30:40 +0900

    [Add / 1주차] test_background.py // 테스트용 배경 이미지 클래스 생성

---
+++ Project/test_background.py
@@ -0,0 +1,11 @@
+from pico2d import load_image
+
+class Background:
+    def __init__(self):
+        self.image = load_image('Sprite/background1.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(640,720)
