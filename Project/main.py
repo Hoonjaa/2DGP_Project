f1c210f1691f68051559a09f7297bc8b3c085844
Hoonjaa <gnswp8978@gmail.com>
2025-10-18 19:07:48 +0900

    [Update / 25.10.18] main.py // 월드 초기화 및 업데이트 렌더 함수 추가

--- Project/main.py
+++ Project/main.py
@@ -1,10 +1,27 @@
 from pico2d import *
 
+def reset_world():
+    global world
+
+    world = []
+
+def update_world():
+    pass
+
+def render_world():
+    clear_canvas()
+    update_canvas()
+
+
+
 running = True
 
 open_canvas(1280, 720)
+reset_world()
 
 while running:
-    pass
+    update_world()
+    render_world()
+    delay(0.01)
 
 close_canvas()
