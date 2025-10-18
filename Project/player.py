8c56cf18bda269f18c8c823832196e07eef26dfc
Hoonjaa <gnswp8978@gmail.com>
2025-10-19 01:43:50 +0900

    [Update / 1주차] player.py // 스프라이트의 액션 좌푯값을 각 상태 클래스에서 관리하도록 변경

--- Project/player.py
+++ Project/player.py
@@ -2,16 +2,10 @@ from pico2d import load_image
 
 from state_machine import StateMachine
 
-player_sprite = ( #0: IDLE, 1: WALK, 2: DASH, 3: JUMP
-    ((7, 1835, 49, 46), (65, 1835, 49, 46), (123, 1835, 49, 47), (181, 1835, 49, 48), (239, 1835, 49, 48), (297, 1835, 49, 48), (355, 1835, 49, 48)),
-    ((7, 1777, 54, 41), (70, 1777, 53, 40), (132, 1777, 53, 41), (194, 1777, 53, 41), (256, 1777, 52, 40), (317, 1776, 52, 43)),
-    ((7, 1720, 54, 35), (70, 1720, 74, 37), (153, 1720, 63, 35), (225, 1720, 57, 35), (291, 1720, 56, 35), (356, 1720, 55, 35), (420, 1720, 54, 35)),
-    ((7, 1655, 43, 47), (59, 1655, 43, 47), (111, 1655, 43, 47)),
-)
-
 class Idle:
     def __init__(self, player):
         self.player = player
+        self.action = ((7, 1835, 49, 46), (65, 1835, 49, 46), (123, 1835, 49, 47), (181, 1835, 49, 48), (239, 1835, 49, 48), (297, 1835, 49, 48), (355, 1835, 49, 48))
 
     def enter(self):
         self.player.dir = 0
@@ -20,13 +14,13 @@ class Idle:
         pass
 
     def do(self):
-        self.player.frame = (self.player.frame + 1) % len(self.player.action)
+        self.player.frame = (self.player.frame + 1) % len(self.action)
 
     def draw(self):
         if self.player.face_dir == 1:
-            self.player.image.clip_draw(*self.player.action[self.player.frame], self.player.x, self.player.y, 100, 100)
+            self.player.image.clip_draw(*self.action[self.player.frame], self.player.x, self.player.y, 100, 100)
         if self.player.face_dir == -1:
-            self.player.image.clip_composite_draw(*self.player.action[self.player.frame], 0, '', self.player.x, self.player.y, 100, 100)
+            self.player.image.clip_composite_draw(*self.action[self.player.frame], 0, '', self.player.x, self.player.y, 100, 100)
 
 class Player:
     def __init__(self):
@@ -34,7 +28,6 @@ class Player:
         self.face_dir = 1
         self.dir = 0
 
-        self.action = player_sprite[0]
         self.frame = 0
         self.image = load_image('Sprite/Player.png')
 
