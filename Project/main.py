4ff7a6fb34cd7a0013d19f109710245dd9e9131b
Hoonjaa <gnswp8978@gmail.com>
2025-10-19 00:55:31 +0900

    [Update / 1주차] main.py // 키 입력을 받기 위한 핸들 이벤트 함수 생성

--- Project/main.py
+++ Project/main.py
@@ -2,6 +2,15 @@ from pico2d import *
 from player import Player
 from test_background import Background
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
 def reset_world():
     global world
 
@@ -31,6 +40,7 @@ open_canvas(1280, 720)
 reset_world()
 
 while running:
+    handle_events()
     update_world()
     render_world()
     delay(0.1)
