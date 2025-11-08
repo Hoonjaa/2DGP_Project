1a51a98b79089d7e85439efcc556a05f2b90d117
Hoonjaa <gnswp8978@gmail.com>
2025-11-08 23:13:17 +0900

    [Update / 4주차] 객체 관리를 위한 모듈 생성

---
+++ Project/game_world.py
@@ -0,0 +1,26 @@
+world = [[], [], []]
+
+def add_object(o, depth = 0):
+    world[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    world[depth] += ol
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            del o
+            return
+
+    raise Exception("월드에 존재하지 않는 객체를 삭제하려고 합니다.")
+
+def update():
+    for layer in world:
+        for o in layer:
+            o.update()
+
+def render():
+    for layer in world:
+        for o in layer:
+            o.draw()
