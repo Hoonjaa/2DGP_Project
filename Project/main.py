c070075630b4bf8794a6071fb9f18ec4fa9203b4
Hoonjaa <gnswp8978@gmail.com>
2025-11-09 00:02:50 +0900

    [Update / 4주차] framework를 적용해 기존 main 함수 수정 및 스테이지화

--- Project/main.py
+++ Project/main.py
@@ -1,48 +1,8 @@
-from pico2d import *
-import game_world
-from player import Player
-from test_background import Background
+from pico2d import open_canvas, delay, close_canvas
+import game_framework
 
-def handle_events():
-    global running
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        else:
-            player.handle_event(event)
-
-def reset_world():
-    global player
-
-    background = Background()
-    game_world.add_object(background,0)
-
-    player = Player()
-    game_world.add_object(player,1)
-
-def update_world():
-    game_world.update()
-
-def render_world():
-    clear_canvas()
-    game_world.render()
-    update_canvas()
-
-
-
-running = True
+import first_stage as start_mode
 
 open_canvas(1280, 720)
-reset_world()
-
-while running:
-    handle_events()
-    update_world()
-    render_world()
-    delay(0.01)
-
+game_framework.run(start_mode)
 close_canvas()
