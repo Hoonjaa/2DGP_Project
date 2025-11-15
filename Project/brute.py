5d8984db8bf92b298e90d878cf37df2c68111ade
Hoonjaa <gnswp8978@gmail.com>
2025-11-15 18:02:06 +0900

    [Update / 5주차] Brute Attack 상태 바운딩 박스 생성

--- Project/brute.py
+++ Project/brute.py
@@ -50,9 +50,12 @@ class Attack:
 
     def draw(self):
         self.monster.draw_current(self.action)
+        draw_rectangle(*self.get_bb(), 255, 0, 0)
 
     def get_bb(self):
-        pass
+        x_offset = self.action[self.monster.frame][2] * BRUTE_SIZE_RATE / 2
+        y_offset = self.action[self.monster.frame][3] * BRUTE_SIZE_RATE / 2
+        return (self.monster.x - x_offset, self.monster.y - y_offset, self.monster.x + x_offset, self.monster.y + y_offset)
 
 
 class Run:
@@ -131,7 +134,7 @@ class Brute:
         self.ATTACK = Attack(self)
         self.HIT = Hit(self)
         self.state_machine = StateMachine(
-            self.RUN,
+            self.ATTACK,
             {
                 self.IDLE: {},
                 self.RUN: {},
