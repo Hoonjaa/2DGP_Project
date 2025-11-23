f363315c3842e40596c1d713fc9a2f1e826c1edc
Hoonjaa <gnswp8978@gmail.com>
2025-11-23 14:50:10 +0900

    [Update / 6주차] 좀비의 체력바 구현

--- Project/zombie.py
+++ Project/zombie.py
@@ -1,6 +1,7 @@
 from pico2d import load_image, draw_rectangle
 import game_world
 import game_framework
+from monster_ui import MonsterUI
 from player import Player
 from damage_text import DamageText
 from state_machine import StateMachine
@@ -53,6 +54,7 @@ class Death:
         self.monster.frame = int(self.monster.anim_progress) % len(self.action)
 
         if self.monster.frame == len(self.action) - 1:
+            game_world.remove_object(self.moster.monster_ui)
             game_world.remove_object(self.monster)
 
     def draw(self):
@@ -179,10 +181,15 @@ class Zombie:
     def __init__(self):
         self.x, self.y = 740, 90
         self.hp = 100
+        self.max_hp = 100
 
         self.attack_damage = 10
 
         self.current_state = 'IDLE'
+
+        # 체력 UI
+        self.monster_ui = MonsterUI(self)
+        game_world.add_object(self.monster_ui, 3)
 
         # 애니메이션 관련 변수
         self.frame = 0
