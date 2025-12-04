4d54e963df0cafabcd6c2a64d4a085ee4117fd61
Hoonjaa <gnswp8978@gmail.com>
2025-12-04 20:10:53 +0900

    [Update / 8주차] 4번째 상품 설명1 추가

--- Project/forge_pannel.py
+++ Project/forge_pannel.py
@@ -3,6 +3,35 @@ import game_world
 from arrow import Arrow
 import common
 
+class UltText:
+    font = None
+    explane_font = None
+    price_font = None
+
+    def __init__(self, x=0, y=0):
+        self.x, self.y = x, y
+        self.text = '그림자 스킬 개방'
+        self.explane1 = 'L을 눌러 그림자를'
+        self.explane2 = '소환하고 주변을'
+        self.explane3 = '공격합니다.'
+        self.price = '가격 : 200'
+        if UltText.font == None:
+            UltText.font = load_font('Galmuri14.ttf', 24)
+        if UltText.explane_font == None:
+            UltText.explane_font = load_font('Galmuri14.ttf', 20)
+        if UltText.price_font == None:
+            UltText.price_font = load_font('Galmuri14.ttf', 15)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        UltText.font.draw(self.x, self.y + 100, str(self.text), (0, 0, 0))
+        UltText.explane_font.draw(self.x, self.y + 50, str(self.explane1), (0, 0, 0))
+        UltText.explane_font.draw(self.x, self.y + 27, str(self.explane2), (0, 0, 0))
+        UltText.explane_font.draw(self.x, self.y + 4, str(self.explane3), (0, 0, 0))
+        UltText.price_font.draw(self.x, self.y - 40, str(self.price), (0, 0, 0))
+
 class SlashText2:
     font = None
     price_font = None
@@ -31,7 +60,7 @@ class SlashText:
     def __init__(self, x=0, y=0):
         self.x, self.y = x, y
         self.text = '검기 스킬 개방'
-        self.explane1 = 'k를 눌러 전방에'
+        self.explane1 = 'K를 눌러 전방에'
         self.explane2 = '검기를 발사합니다.'
         self.price = '가격 : 100'
         if SlashText.font == None:
@@ -100,6 +129,7 @@ class ForgePannel:
         self.heart_text = HeartText(833, 500)
         self.slash_text = SlashText(400, 200)
         self.slash_text2 = SlashText2(400, 200)
+        self.ult_text = UltText(833, 200)
 
     def draw(self):
         self.image.draw(640, 360)
@@ -107,6 +137,7 @@ class ForgePannel:
         self.heart_text.draw()
         if common.player.is_slash_unlocked == False: self.slash_text.draw()
         else: self.slash_text2.draw()
+        if common.player.is_ult_unlocked == False: self.ult_text.draw()
 
     def update(self):
         self.arrow.change_position(*self.arrow_positions[self.current_selection])
