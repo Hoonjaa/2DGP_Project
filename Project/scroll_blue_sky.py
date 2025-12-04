5110cdffa0c06ff95478dedd46fddf3187637244
Hoonjaa <gnswp8978@gmail.com>
2025-12-04 22:17:20 +0900

    [Update / 8주차] 하늘의 스크롤링을 위한 함수 수정

--- Project/scroll_blue_sky.py
+++ Project/scroll_blue_sky.py
@@ -6,12 +6,32 @@ class ScrollBlueSky:
         self.image = load_image('Sprite/sky.png')
         self.cw = get_canvas_width()
         self.ch = get_canvas_height()
-        self.w = self.image.w
+        self.tile_w = self.image.w
+        self.w = 7488
         self.h = self.image.h
+
+        # 초기 윈도우 위치 설정
+        self.window_left = 0
+        self.window_bottom = 0
 
     def update(self):
         self.window_left = clamp(0, int(common.player.x) - self.cw // 2, self.w - self.cw - 1)
         self.window_bottom = clamp(0, int(common.player.y) - self.ch // 2, self.h - self.ch - 1)
 
     def draw(self):
-        self.image.clip_draw_to_origin(self.window_left, self.window_bottom, self.cw, self.ch, 0, 0)
+        start_tile = self.window_left // self.tile_w
+        num_tiles = (self.cw // self.tile_w) + 2
+        for i in range(num_tiles):
+            tile_idx = start_tile + i
+            if tile_idx * self.tile_w >= self.w:
+                break
+
+            # 타일의 월드 좌표
+            tile_world_x = tile_idx * self.tile_w
+            # 화면 좌표로 변환
+            tile_screen_x = tile_world_x - self.window_left
+
+            if tile_idx % 2 == 0:
+                self.image.clip_draw_to_origin(0, self.window_bottom, self.tile_w, self.ch, tile_screen_x, 0)
+            else: # 반전된 이미지 그리기
+                self.image.clip_composite_draw(0, self.window_bottom, self.tile_w, self.ch, 0, 'h', tile_screen_x + self.tile_w // 2, self.ch // 2, self.tile_w, self.ch)
