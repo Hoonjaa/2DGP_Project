07fb8576fb8f446489cae05e10d34ba220f9d244
Hoonjaa <gnswp8978@gmail.com>
2025-12-04 18:40:34 +0900

    [Update / 8주차] 판넬이 생성될 때, 화살표도 소환

--- Project/forge_pannel.py
+++ Project/forge_pannel.py
@@ -1,11 +1,19 @@
 from pico2d import *
+import game_world
+from arrow import Arrow
 
 class ForgePannel:
     def __init__(self):
         self.image = load_image('Sprite/forge_ui.png')
+        self.arrow_positions = ((200, 500), (400, 500), (600, 500), (800, 500)) # 임시값
+        self.arrow = Arrow(*self.arrow_positions[0])
+        game_world.add_object(self.arrow,3)
 
     def draw(self):
         self.image.draw(640, 360)
 
     def update(self):
+        pass
+
+    def handle_event(self, event):
         pass
