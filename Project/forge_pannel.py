e77a3f3332165caa8fd0e7644bc9e37be63e09d0
Hoonjaa <gnswp8978@gmail.com>
2025-12-04 19:41:56 +0900

    [Update / 8주차] 세번째 상품 설명 추가

--- Project/forge_pannel.py
+++ Project/forge_pannel.py
@@ -2,6 +2,33 @@ from pico2d import *
 import game_world
 from arrow import Arrow
 
+class SlashText:
+    font = None
+    explane_font = None
+    price_font = None
+
+    def __init__(self, x=0, y=0):
+        self.x, self.y = x, y
+        self.text = '검기 스킬 개방'
+        self.explane1 = 'k를 눌러 전방에'
+        self.explane2 = '검기를 발사합니다.'
+        self.price = '가격 : 100'
+        if SlashText.font == None:
+            SlashText.font = load_font('Galmuri14.ttf', 24)
+        if SlashText.explane_font == None:
+            SlashText.explane_font = load_font('Galmuri14.ttf', 20)
+        if SlashText.price_font == None:
+            SlashText.price_font = load_font('Galmuri14.ttf', 15)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        SlashText.font.draw(self.x, self.y + 100, str(self.text), (0, 0, 0))
+        SlashText.explane_font.draw(self.x, self.y + 50, str(self.explane1), (0, 0, 0))
+        SlashText.explane_font.draw(self.x, self.y + 27, str(self.explane2), (0, 0, 0))
+        SlashText.price_font.draw(self.x, self.y - 40, str(self.price), (0, 0, 0))
+
 class HeartText:
     font = None
     price_font = None
@@ -50,11 +77,13 @@ class ForgePannel:
 
         self.attack_text = AttackText(400, 500)
         self.heart_text = HeartText(833, 500)
+        self.slash_text = SlashText(400, 200)
 
     def draw(self):
         self.image.draw(640, 360)
         self.attack_text.draw()
         self.heart_text.draw()
+        self.slash_text.draw()
 
     def update(self):
         self.arrow.change_position(*self.arrow_positions[self.current_selection])
