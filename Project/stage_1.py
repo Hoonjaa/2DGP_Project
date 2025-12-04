11935e9121677853ab49c41253fcd0e826fea93d
Hoonjaa <gnswp8978@gmail.com>
2025-12-04 21:25:54 +0900

    [Update / 8주차] 스테이지 1 준비

---
+++ Project/stage_1.py
@@ -0,0 +1,52 @@
+from pico2d import *
+import game_world
+import game_framework
+import common
+from player_ui import PlayerUI
+from brute import Brute
+from player import Player
+from zombie import Zombie
+from variant_zombie2 import VZ2
+from variant_zombie1 import VZ1
+from boss import Boss
+
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            common.player.handle_event(event)
+
+def init():
+
+    if common.player is None:
+        common.player = Player()
+    common.player.x = 100
+    game_world.add_object(common.player,1)
+    game_world.add_collision_pair('player:monster_attack', common.player, None)
+
+    player_ui = PlayerUI(common.player)
+    game_world.add_object(player_ui,3)
+
+def update():
+    game_world.update()
+    game_world.handle_collisions()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    game_world.clear()
+
+def pause():
+    pass
+
+def resume():
+    pass
