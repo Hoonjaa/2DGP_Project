302f6c568464862b71cf86379f369ce9aed96305
Hoonjaa <gnswp8978@gmail.com>
2025-12-06 02:10:51 +0900

    [Update / 8주차] 보스 스테이지 사운드 추가

--- Project/castle_background.py
+++ Project/castle_background.py
@@ -1,8 +1,11 @@
-from pico2d import load_image
+from pico2d import load_image, load_music
 
 class CastleBackground:
     def __init__(self):
         self.image = load_image('Sprite/castle.png')
+        self.bgm = load_music('Sound/boss.mp3')
+        self.bgm.set_volume(25)
+        self.bgm.repeat_play()
 
     def update(self):
         pass
